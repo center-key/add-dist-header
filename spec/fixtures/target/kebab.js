00871deb39286f2e8156d11b5afa2457d81a17cf
dpilafian <dem@centerkey.com>
2023-02-16 02:34:43 -0800

    Release v1.0.0

--- spec/fixtures/target/kebab.js
+++ spec/fixtures/target/kebab.js
@@ -1,8 +1,8 @@
-//! add-dist-header v0.3.8 🫓🍢🫓 https://github.com/center-key/add-dist-header 🫓🍢🫓 MIT License
+//! add-dist-header v1.0.0 🫓🍢🫓 https://github.com/center-key/add-dist-header 🫓🍢🫓 MIT License
 
-const toKebab = (camelStr) => {  //v0.3.8
+const toKebab = (camelStr) => {  //v1.0.0
    const dash = (word) => '-' + word.toLowerCase();
    return ('' + camelStr).replace(/([A-Z]+)/g, dash).replace(/\s|^-/g, '');
    };
 
-module.exports = toKebab;  //version: 0.3.8
+module.exports = toKebab;  //version: 1.0.0
--- spec/fixtures/target/kebab.ts
+++ spec/fixtures/target/kebab.ts
@@ -1,4 +1,4 @@
-//! add-dist-header v0.3.8 ~~ https://github.com/center-key/add-dist-header ~~ MIT License
+//! add-dist-header v1.0.0 ~~ https://github.com/center-key/add-dist-header ~~ MIT License
 
 //! The toKebab function is important (and don't substitute the version number)
 const toKebab = (camelStr: string): string => {  //v{{pkg.version}}
