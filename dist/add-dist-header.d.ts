3bb8a7fdecfdc65543c20c2b4df12f5027243ab6
dpilafian <dem@centerkey.com>
2023-03-20 14:04:56 -0700

    Release v1.0.1

--- dist/add-dist-header.d.ts
+++ dist/add-dist-header.d.ts
@@ -1,4 +1,4 @@
-//! add-dist-header v1.0.0 ~~ https://github.com/center-key/add-dist-header ~~ MIT License
+//! add-dist-header v1.0.1 ~~ https://github.com/center-key/add-dist-header ~~ MIT License
 
 export type Settings = {
     dist: string;
