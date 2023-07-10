9bb3bd6334bddfda152947419d641479b4ae298f
dpilafian <dem@centerkey.com>
2023-07-10 16:04:11 -0700

    Put header comment after doctype in html and xml

--- add-dist-header.ts
+++ add-dist-header.ts
@@ -37,6 +37,7 @@ const addDistHeader = {
       const settings = { ...defaults, ...options };
       if (!filename)
          throw Error('[add-dist-header] Must specify the "filename" option.');
+      const doctypeLine = /^<(!doctype|\?xml).*\n/i;  //matches: '<!doctype html>' and '<?xml version="1.0" ?>'
       const commentStyle = {
          js:    { start: '//! ',  end: '' },
          ml:    { start: '<!-- ', end: ' -->' },
@@ -59,8 +60,10 @@ const addDistHeader = {
       const normalizeEof =   /\s*$(?!\n)/;
       const out1 =           input.replace(normalizeEol, '').replace(normalizeEof, '\n');
       const out2 =           settings.replaceComment ? out1.replace(firstLine[type], '') : out1;
+      const doctype =        mlStyle && out2.match(doctypeLine)?.[0] || '';
+      const out3 =           mlStyle && doctype ? out2.replace(doctype, '') : out2;
       const versionPattern = /{{pkg[.]version}}/g;
-      const out3 =           settings.setVersion ? out2.replace(versionPattern, pkg.version) : out2;
+      const out4 =           settings.setVersion ? out3.replace(versionPattern, pkg.version) : out3;
       const info =           pkg.homepage ?? pkg.docs ?? pkg.repository;
       const unlicensed =     !pkg.license || pkg.license === 'UNLICENSED';
       const license =        unlicensed ? 'All Rights Reserved' : pkg.license + ' License';
@@ -70,10 +73,10 @@ const addDistHeader = {
       const fixedDigits =    { minimumFractionDigits: 2, maximumFractionDigits: 2 };
       const distFolder =     makeDir.sync(settings.dist);
       const outputPath =     slash(path.format({ dir: settings.dist, name: inputFile.name, ext: fileExt }));
-      const isMinified =     outputPath.includes('.min.') || out3.indexOf('\n') === out3.length - 1;
+      const isMinified =     outputPath.includes('.min.') || out4.indexOf('\n') === out4.length - 1;
       const spacerLines =    isMinified || mlStyle ? '\n' : '\n\n';
       const leadingBlanks =  /^\s*\n/;
-      const final =          header + spacerLines + out3.replace(leadingBlanks, '');
+      const final =          doctype + header + spacerLines + out4.replace(leadingBlanks, '');
       if (!invalidContent)
          fs.writeFileSync(outputPath, final);
       return {
