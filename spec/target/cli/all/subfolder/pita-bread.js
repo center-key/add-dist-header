1b2a641c5265cbca58e22bae305b961ca6202756
pilafmon <pilaf@centerkey.com>
2026-04-21 22:58:58 -0700

    Release v1.6.4

--- spec/target/cli/all/subfolder/pita-bread.js
+++ spec/target/cli/all/subfolder/pita-bread.js
@@ -1,8 +1,8 @@
-//! add-dist-header v1.6.3 ~~ https://github.com/center-key/add-dist-header ~~ MIT License
+//! add-dist-header v1.6.4 ~~ https://github.com/center-key/add-dist-header ~~ MIT License
 
 //! This comment is important!
 import { toKebab } from '../kebab';
 
-const pitaBread = (str) => `🫓 ${toKebab(str)} 🫓`;  //v1.6.3
+const pitaBread = (str) => `🫓 ${toKebab(str)} 🫓`;  //v1.6.4
 
 export { pitaBread };
