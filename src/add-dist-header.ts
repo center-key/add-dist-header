d91df12df7fd61fd45e2f8e228e80b9cd970afbe
pilafmon <pilaf@centerkey.com>
2026-08-17 14:37:21 -0700

    Simplify log() parameters

--- src/add-dist-header.ts
+++ src/add-dist-header.ts
@@ -140,9 +140,10 @@ const addDistHeader = {
          };
       const settings =  { ...defaults, ...options };
       const fileCount = count ? chalk.magenta(count) + ' ' : '';
-      const size =      chalk.gray('(' + (result.size || 'binary') + ')');
+      const fileInfo =  fileCount + cliArgvUtil.colorizePath(result.file);
+      const size =      chalk.blue(`(${result.size || 'binary'})`);
       if (!settings.quiet && result.valid)
-         log(name, fileCount + cliArgvUtil.colorizePath(result.file), size);
+         log(name, fileInfo, size);
       return result;
       },
 
